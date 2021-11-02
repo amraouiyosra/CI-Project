419c206cffb1cb39c3da60bfef4112e375f9722e
amraouiyosra <yosra.amraoui@esprit.tn>
2021-11-02 18:13:00 +0100

    webhook test6

--- src/test/java/tn/esprit/spring/service/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/EntrepriseServiceImplTest.java
@@ -61,8 +61,8 @@ public class EntrepriseServiceImplTest {
 	
 	@Test
 	public void testDeleteEntreprise() {
-		en.deleteEntreprise("49");
-		Assert.assertNull(en.retrieveEntreprise("49"));
+		en.deleteEntreprise("50");
+		Assert.assertNull(en.retrieveEntreprise("50"));
 	}
 
 	// 5 tests unitaires  
